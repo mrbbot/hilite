595cc10d2283919a7d7c3657a060bc2c5d156719
mrbbot <me@mrbbot.co.uk>
2018-07-18 08:51:20 +0100

    Image support

--- src/highlight.js
+++ src/highlight.js
@@ -4,6 +4,8 @@ const languages = require("./languages");
 require("prismjs/components/")(languages.map(language => language.name));
 const files = require("./files");
 
+const images = ["png", "jpg", "jpeg", "gif", "svg"];
+
 async function highlight(globs, progressCallback) {
   progressCallback("glob", 0, "");
   const matched = await files(globs);
@@ -25,30 +27,36 @@ async function highlight(globs, progressCallback) {
       return result;
     })
     .map((file, i, arr) => {
-      // noinspection JSUnresolvedFunction, JSUnresolvedVariable
-      const result = {
-        ...file,
-        highlighted: file.language
-          ? prism.highlight(
-              file.content,
-              prism.languages[file.language],
-              file.language
-            )
-          : file.content
-      };
+      let result;
+
+      if (images.includes(file.extension)) {
+        result = {
+          ...file,
+          result: `<img src="${file.name}">`
+        };
+      } else {
+        // noinspection JSUnresolvedFunction, JSUnresolvedVariable
+        result = {
+          ...file,
+          result: `<pre class="language-${
+            file.language
+          }"><code class="language-${file.language}">${
+            file.language
+              ? prism.highlight(
+                  file.content,
+                  prism.languages[file.language],
+                  file.language
+                )
+              : file.content
+          }</code></pre>`
+        };
+      }
 
       progressCallback("highlight", 0.4 + (i / arr.length) * 0.2, file.name);
 
       return result;
     })
-    .map(
-      file =>
-        `<h1>${file.name}</h1><pre class="language-${
-          file.language
-        }"><code class="language-${file.language}">${
-          file.highlighted
-        }</code></pre>`
-    )
+    .map(file => `<h1>${file.name}</h1>${file.result}`)
     .join("");
 }
 
